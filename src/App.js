d72fbb8716e1cea54c27d9966ed5beac17db1976
Maksym Chubarov <maximuskh@gmail.com>
2021-04-14 22:46:48 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Suspense, useEffect, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { useDispatch, connect } from 'react-redux';
-import HomeView from './views/HomeView';
+// import HomeView from './views/HomeView';
+import ContactsView from './views/ContactsView/ContactsView';
+// import RegisterView from './views/RegisterView/RegisterView';
+// import LoginView from './views/LoginView/LoginView';
+import Container from './components/Container/Container';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import PublicRoute from './components/PublicRoute/PublicRoute';
-import ContactsView from './views/ContactsView/ContactsView';
-import RegisterView from './views/RegisterView/RegisterView';
-import LoginView from './views/LoginView/LoginView';
 import AppBar from './components/AppBar/AppBar';
 import { getCurrentUser } from './redux/auth/auth-operations';
-
 import * as contactSelectors from '../src/redux/contact/contact-selectors';
 
-const App = ({ loading }) => {
+const App = () => {
+  const HomeView = lazy(() =>
+    import('./views/HomeView' /* webpackChunkName: "home-view" */)
+  );
+
+  // const ContactsView = lazy(() =>
+  //   import(
+  //     './views/ContactsView/ContactsView' /* webpackChunkName: "contacts-view" */
+  //   )
+  // );
+
+  const RegisterView = lazy(() =>
+    import(
+      './views/RegisterView/RegisterView' /* webpackChunkName: "register-view" */
+    )
+  );
+
+  const LoginView = lazy(() =>
+    import('./views/LoginView/LoginView' /* webpackChunkName: "login-view" */)
+  );
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -21,23 +41,25 @@ const App = ({ loading }) => {
 
   return (
     <>
-      <Suspense fallback={<p>Loading...</p>}>
-        <AppBar />
-        <Switch>
-          <PublicRoute exact path="/" restricted redirectTo="/contacts">
-            <HomeView />
-          </PublicRoute>
-          <PrivateRoute path="/contacts" redirectTo="/login">
-            <ContactsView />
-          </PrivateRoute>
-          <PublicRoute path="/register" restricted redirectTo="/contacts">
-            <RegisterView />
-          </PublicRoute>
-          <PublicRoute path="/login" restricted redirectTo="/contacts">
-            <LoginView />
-          </PublicRoute>
-        </Switch>
-      </Suspense>
+      <Container>
+        <Suspense fallback={<p>Loading...</p>}>
+          <AppBar />
+          <Switch>
+            <PublicRoute exact path="/" restricted redirectTo="/contacts">
+              <HomeView />
+            </PublicRoute>
+            <PrivateRoute path="/contacts" redirectTo="/login">
+              <ContactsView />
+            </PrivateRoute>
+            <PublicRoute path="/register" restricted redirectTo="/contacts">
+              <RegisterView />
+            </PublicRoute>
+            <PublicRoute path="/login" restricted redirectTo="/contacts">
+              <LoginView />
+            </PublicRoute>
+          </Switch>
+        </Suspense>
+      </Container>
     </>
   );
 };
--- src/views/RegisterView/RegisterView.js
+++ src/views/RegisterView/RegisterView.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { signup } from '../../redux/auth/auth-operations';
+import style from './RegisterView.module.css';
 
 const RegisterView = () => {
   const dispatch = useDispatch();
-  const [name, setName] = useState('Name');
-  const [email, setEmail] = useState('name@mail.ru');
-  const [password, setPassword] = useState('QW1234!');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleChangeName = (e) => setName(e.target.value);
   const handleChangeEmail = (e) => setEmail(e.target.value);
@@ -17,42 +18,45 @@ const RegisterView = () => {
 
     dispatch(signup({ name, email, password }));
 
-    // onSubmit(name, email, password);
-    // setName('');
-    // setEmail('');
-    // setPassword('');
+    setName('');
+    setEmail('');
+    setPassword('');
   };
 
   return (
     <div>
       <h2>RegisterView</h2>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Username"
-          onChange={handleChangeName}
-          value={name}
-        />
-        Username
-        <label>
+      <form onSubmit={handleSubmit} className={style.form}>
+        <label className={style.label}>
+          Username
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={handleChangeName}
+            value={name}
+          />
+        </label>
+        <label className={style.label}>
+          Email address
           <input
             type="email"
             name="email"
+            placeholder="Email"
             value={email}
             onChange={handleChangeEmail}
           />
-          Email address
         </label>
-        <label>
+        <label className={style.label}>
+          Password
           <input
             type="password"
             name="password"
+            placeholder="Password"
             value={password}
             onChange={handleChangePassword}
           />
-          Password
         </label>
         <button type="submit">Enter</button>
       </form>
@@ -61,3 +65,7 @@ const RegisterView = () => {
 };
 
 export default RegisterView;
+
+// const [name, setName] = useState('Name');
+// const [email, setEmail] = useState('name@mail.ru');
+// const [password, setPassword] = useState('QW1234!');
