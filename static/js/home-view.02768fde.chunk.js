b5569fca313fad5ce9945f0e19cb8b2ce59c560e
Maksym Chubarov <maximuskh@gmail.com>
2021-04-14 22:47:59 +0300

    Updates

---
+++ static/js/home-view.02768fde.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[0],{90:function(e,t,o){"use strict";o.r(t);var c=o(1);t.default=function(){return Object(c.jsx)("div",{children:Object(c.jsx)("h1",{children:" Welcome!!!"})})}}}]);
+//# sourceMappingURL=home-view.02768fde.chunk.js.map
